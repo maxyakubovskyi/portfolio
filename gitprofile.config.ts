9b9615183574a87c8c737584a7f2a3dd0f5cbdb4
Maksym Yakubovskyi <off.maxme@gmail.com>
2026-05-01 21:11:17 +0000

    replaced the name

--- gitprofile.config.ts
+++ gitprofile.config.ts
@@ -51,7 +51,7 @@ const CONFIG = {
       ],
     },
   },
-  seo: { title: 'Portfolio of Ariful Alam', description: '', imageURL: '' },
+  seo: { title: 'Portfolio of Max Yakubovskyi', description: '', imageURL: '' },
   social: {
     linkedin: 'мах-yakubovskiy-128769407',
     x: '',
